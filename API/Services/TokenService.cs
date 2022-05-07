727bb69585140ea870d267a829314a2520c551d0
Michael Lauritsen <i3028841@gmail.com>
2022-05-07 15:11:33 +0200

    add token service

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,13 @@
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string GenerateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
